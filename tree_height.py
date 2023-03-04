e1ac70573d084674f46b240027532752c17cd27a
Eduards Janītis (221RDB271) <118062580+EduardsJ1@users.noreply.github.com>
2023-03-04 23:54:31 +0000

    commit

--- tree_height.py
+++ tree_height.py
@@ -6,20 +6,44 @@ import threading
 
 def compute_height(n, parents):
     # Write this function
+    #print(n,parents)
     max_height = 0
+    height={}
+    for i in range(n):
+        height[i]=[]
     # Your code here
+    for i in range(n):
+        if parents[i]==-1:
+            root=i
+        else:
+            if parents[i] >= 0 and parents[i]<n:
+                height[parents[i]].append(i)
+
+    def fheight(i):
+        if len(height[i])==0:
+            return 1
+        else:
+            return 1 + max(fheight(j) for j in height[i])
+            
+
+    
+
+    #print(height)
+    max_height=fheight(root)
     return max_height
 
 
 def main():
     # implement input form keyboard and from files
-    
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
     
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
+    n=int(input())
+    parents=list(map(int,input().split()))
     # call the function and output it's result
+    print(compute_height(n,parents))
 
 
 # In Python, the default limit on recursion depth is rather low,
